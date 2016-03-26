5f3be05569afae30ff0d4bef0e99a2fe3a4135d1
Zhenghui Li <leezhenghui@gmail.com>
2016-03-26 18:22:25 +0800

    clean up script used for predefined kernel

--- device.mk
+++ device.mk
@@ -8,14 +8,6 @@ $(call inherit-product-if-exists, vendor/emulator/twrp/twrp-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/emulator/twrp/overlay
 
 LOCAL_PATH := device/emulator/twrp
-# ifeq ($(TARGET_PREBUILT_KERNEL),)
-# 	LOCAL_KERNEL := $(LOCAL_PATH)/kernAl
-# else
-# 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-# endif
-# 
-# PRODUCT_COPY_FILES += \
-#     $(LOCAL_KERNEL):kernel
 
 $(call inherit-product, build/target/product/full.mk)
 
